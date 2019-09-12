b6e2253e6d45c4ba76eb41800ccc05ecea537b1a
Hyeeyoung Kim <hyeeyoung.kim@gmail.com>
2019-09-12 16:24:21 -0400

    Add additional argument and logic for metadata export

--- baroque.py
+++ baroque.py
@@ -1,12 +1,12 @@
 import argparse
 
 from baroque.baroque_project import BaroqueProject
-from baroque.directory_validation import validate_directories
-from baroque.mets_validation import validate_mets
-from baroque.wav_bext_chunk_validation import validate_wav_bext_chunks
-from baroque.file_format_validation import validate_file_formats
 from baroque.checksum_validation import validate_checksums
+from baroque.directory_validation import validate_directories
+from baroque.file_format_validation import validate_file_formats
+from baroque.mets_validation import validate_mets
 from baroque.report_generation import generate_reports
+from baroque.wav_bext_chunk_validation import validate_wav_bext_chunks
 
 
 def main():
@@ -14,15 +14,19 @@ def main():
     parser.add_argument("source", help="Path to source directory")
     parser.add_argument("destination", help="Path to destination for reports")
     parser.add_argument("-d", "--directories", action="store_true", help="Validate directories")
+    parser.add_argument("-e", "--export", help="Path to metadata export")
     parser.add_argument("-m", "--mets", action="store_true", help="Validate METS")
     parser.add_argument("-w", "--wav", action="store_true", help="Validate WAV BEXT chunks")
     parser.add_argument("-f", "--files", action="store_true", help="Validate file formats")
     parser.add_argument("-c", "--checksums", action="store_true", help="Validate checksums")
     args = parser.parse_args()
 
+    if args.directories and not args.export:
+        parser.error("--export is required for --directories.")
+
     project = BaroqueProject(args.source, args.destination)
     if args.directories:
-        validate_directories(project)
+        validate_directories(project, args.export)
     if args.mets:
         validate_mets(project)
     if args.wav:
