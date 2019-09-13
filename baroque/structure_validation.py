92563289db44887d39ed7324ef2b4769848ca494
Hyeeyoung Kim <hyeeyoung.kim@gmail.com>
2019-09-13 09:38:14 -0400

    Update functions to support different source directory level

--- baroque/structure_validation.py
+++ baroque/structure_validation.py
@@ -24,20 +24,19 @@ def parse_metadata_export(metadata_export, level):
 
     with open(metadata_export, "r", newline="") as f:
         reader = csv.DictReader(f)
-        if level == "collections":
-            for row in reader:
-                collection_id = row.get("DigFile Calc").strip().split("-")[0]
-                if collection_id not in collection_ids:
-                    collection_ids.append(collection_id)
+        for row in reader:
+            item_id = row.get("DigFile Calc").strip()
+            items_ids.append(item_id)
 
-            return collection_ids
+    if level == "collections":
+        for i in items_ids:
+            collection_id = i.split("-")[0]
+            if collection_id not in collection_ids:
+                collection_ids.append(collection_id)
+        return collection_ids
 
-        elif level == "items":
-            for row in reader:
-                item_id = row.get("DigFile Calc").strip()
-                items_ids.append(item_id)
-
-            return items_ids
+    elif level == "items":
+        return items_ids
 
 
 def compare_ids(set_a_ids, set_b_ids, level):
@@ -48,17 +47,27 @@ def compare_ids(set_a_ids, set_b_ids, level):
 
 def check_empty_directory(directory_path):
     if len(os.listdir(directory_path)) == 0:
-        print("ERROR: " + os.path.basename(directory_path) + " is empty")
+        print("ERROR: " + os.path.basename(directory_path) + " is empty directory")
+
+
+def validate_directory(baroqueproject, metadata_export, level):
+    process_ids, process_paths = parse_baroqueproject(baroqueproject, level)
+    export_ids = parse_metadata_export(metadata_export, level)
+
+    compare_ids(process_ids, export_ids, level)
+
+    for path in process_paths:
+        check_empty_directory(path)
 
 
 def validate_structure(baroqueproject, metadata_export):
-    levels = ["collections", "items"]
+    print("This is...", baroqueproject.source_type, "!!!")
 
-    for level in levels:
-        process_ids, process_paths = parse_baroqueproject(baroqueproject, level)
-        export_ids = parse_metadata_export(metadata_export, level)
+    if baroqueproject.source_type == "shipment":
+        levels = ["collections", "items"]
+        for level in levels:
+            validate_directory(baroqueproject, metadata_export, level)
 
-        compare_ids(process_ids, export_ids, level)
-
-        for path in process_paths:
-            check_empty_directory(path)
+    elif baroqueproject.source_type == "collection":
+        level = "items"
+        validate_directory(baroqueproject, metadata_export, level)
