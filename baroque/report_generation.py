f8c94765e8dd22348a96a023fbfc58f61fcebc19
Liz Gadelha <gadelha@umich.edu>
2019-09-26 09:03:44 -0400

    Incorporate report_generation feedback
    Co-Authored-By: @djpillen <djpillen@umich.edu>

--- baroque/report_generation.py
+++ baroque/report_generation.py
@@ -1,14 +1,17 @@
 import csv
-import datetime
+import os
+
+from datetime import datetime
 
 
 def generate_reports(baroqueproject):
     # Create the csv file name with the following structure: directory-YYYYMMDD-HHMMSS.csv.
-    date = (str(datetime.datetime.now())).split(".")[0].replace("-","").replace(":","").replace(" ","-")
-    source = baroqueproject.source_directory
+    date = datetime.now().strftime("%Y%H%m-%H%M%S")
+    source = os.path.basename(baroqueproject.source_directory)
     csv_filename = source + "-" + date + ".csv"
+    csv_filepath = os.path.join(baroqueproject.destination_directory, csv_filename)
 
-    with open(csv_filename, 'w', newline='') as csvfile:
+    with open(csv_filepath, 'w', newline='') as csvfile:
         # Define and write header row values of csv file.
         fieldnames = ['validation', 'error_type', 'path', 'id', 'error']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
