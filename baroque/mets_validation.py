20b7f6667ed58d327c2c42f4fcd13d8bca5532d7
Max Eckard <eckardm@umich.edu>
2019-09-30 14:22:08 -0400

    Parse item METS

--- baroque/mets_validation.py
+++ baroque/mets_validation.py
@@ -1,3 +1,21 @@
+import os
+from lxml import etree
+from pprint import pprint
+
+def parse_item_mets(item):
+    path = item['path']
+
+    # Assuming for now that validating directory and file structure would have picked this up
+    if not item['files']['xml']:
+        return None
+    
+    else:
+        mets = item['files']['xml'][0]
+        tree = etree.parse(os.path.join(path, mets))
+        return tree
+
 def validate_mets(BaroqueProject):
-    """ Validates METS """
-    pass
+    """ 
+    Validates METS"""
+    for item in BaroqueProject.items:
+        tree = parse_item_mets(item)
