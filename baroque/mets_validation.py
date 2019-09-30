0a3ee78ad8273e61a3380dd589b7c56f034c7d9a
Max Eckard <eckardm@umich.edu>
2019-09-30 15:22:27 -0400

    Parse METS header

--- baroque/mets_validation.py
+++ baroque/mets_validation.py
@@ -1,21 +1,69 @@
 import os
 from lxml import etree
-from pprint import pprint
 
-def parse_item_mets(item):
-    path = item['path']
-
-    # Assuming for now that validating directory and file structure would have picked this up
-    if not item['files']['xml']:
-        return None
+def parse_mets_header(item_id, path_to_mets, tree):
+    """
+    Validates metsHdr"""
+    namespaces = {
+        'mets': 'http://www.loc.gov/METS/'
+    }
+    
+    if not tree.xpath("/mets:mets/mets:metsHdr", namespaces=namespaces):
+        errors =[
+            "mets_validation",
+            "requirement",
+            path_to_mets,
+            item_id,
+            "mets xml has no mets header"
+        ]
+        print('SYSTEM ERROR:' + str(errors))
     
     else:
-        mets = item['files']['xml'][0]
-        tree = etree.parse(os.path.join(path, mets))
-        return tree
+        mets_header = tree.xpath("/mets:mets/mets:metsHdr", namespaces=namespaces)[0]
+        return mets_header
+
+def parse_descriptive_metadata(item_id, path_to_mets, tree):
+    """
+    Validates dmdSec"""
+    pass
+
+def parse_administrative_metadata(item_id, path_to_mets, tree):
+    """
+    Validates amdSec"""
+    pass
+
+def parse_file_section(item_id, path_to_mets, tree):
+    """
+    Validates fileSec"""
+    pass
+
+def parse_structural_map_section(item_id, path_to_mets, tree):
+    """
+    Validates structMap"""
+    pass
+
+def parse_item_mets(item):
+    path_to_item = item['path']
+    item_id = item['id']
+    mets = item['files']['xml'][0]
+    path_to_mets = os.path.join(path_to_item, mets)
+    tree = etree.parse(path_to_mets)
+    
+    return item_id, path_to_mets, tree
 
 def validate_mets(BaroqueProject):
     """ 
     Validates METS"""
     for item in BaroqueProject.items:
-        tree = parse_item_mets(item)
+    
+        # Assuming for now that validating directory and file structure would have picked this up
+        if not item['files']['xml']:
+            continue
+
+        item_id, path_to_mets, tree = parse_item_mets(item)
+        
+        parse_mets_header(item_id, path_to_mets, tree)
+        parse_descriptive_metadata(item_id, path_to_mets, tree)
+        parse_administrative_metadata(item_id, path_to_mets, tree)
+        parse_file_section(item_id, path_to_mets, tree)
+        parse_structural_map_section(item_id, path_to_mets, tree)
