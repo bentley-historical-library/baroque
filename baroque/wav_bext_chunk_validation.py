de536b5a14255516cfce59fb9f0ebcaac732d9c6
Max Eckard <eckardm@umich.edu>
2019-10-21 15:59:10 -0400

    get paths to wavs

--- baroque/wav_bext_chunk_validation.py
+++ baroque/wav_bext_chunk_validation.py
@@ -1,3 +1,6 @@
+import os
+from tqdm import tqdm
+
 from .baroque_validator import BaroqueValidator
 
 class WavBextChunkValidator(BaroqueValidator):
@@ -5,7 +8,19 @@ class WavBextChunkValidator(BaroqueValidator):
         validation = "wav_bext_chunk"
         validator = self.validate_wav_bext_chunks
         super().__init__(validation, validator, project)
-
+    
+    def get_paths_to_wavs(self, item):
+        paths_to_wavs = []
+        path_to_item = item['path']
+        wav_files = item['files']['wav']
+        for wav_file in wav_files:
+            paths_to_wavs.append(os.path.join(path_to_item, wav_file))
+        
+        return paths_to_wavs
+    
     def validate_wav_bext_chunks(self):
-        """ Validates WAV BEXT chunks """
-        pass
+        """ 
+        Validates WAV BEXT chunks """
+
+        for item in tqdm(self.project.items):
+            paths_to_wavs = self.get_paths_to_wavs(item)
