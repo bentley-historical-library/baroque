56ea519b33a2ed0de044f6a64404c81656dcd47c
Max Eckard <eckardm@umich.edu>
2019-10-22 13:57:34 -0400

    Implements 'first pass' WAV BEXT chunk validation

--- baroque/wav_bext_chunk_validation.py
+++ baroque/wav_bext_chunk_validation.py
@@ -1,6 +1,7 @@
-import json
+import csv
 import os
 import subprocess
+import io
 from tqdm import tqdm
 
 from .baroque_validator import BaroqueValidator
@@ -22,84 +23,56 @@ class WavBextChunkValidator(BaroqueValidator):
         
         return paths_to_wavs
 
-    def get_mediainfo_json(self, path_to_wav):
+    def get_bwfmetaedit_csv(self, path_to_wav):
         """
-        Uses Windows 64-bit MediaInfo CLI to return JSON:
-
-        {'media': {'@ref': 'R:\\BAroQUe\\Transcription Discs '
-                   'B2\\2018001\\86746\\86746-SR-10\\86746-SR-10-1-pm.wav',
-           'track': [{'@type': 'General',
-                      'AudioCount': '1',
-                      'Description': 'WXYZ Michigan Radio Network, disc 3',
-                      'Duration': '309.267',
-                      'EncodedBy': 'Russak, Laura',
-                      'Encoded_Date': '2018-08-23 14:51:51',
-                      'Encoded_Library_Settings': 'A=ANALOGUE,M=mono,T=Technics '
-                                                  'SL1210-MK2; 78 RPM; grooved '
-                                                  'disc / '
-                                                  'A=PCM,F=96000,W=24,M=mono,T=Antelope '
-                                                  'Audio;Orion 32;A/D / '
-                                                  'A=PCM,F=96000,W=24,M=mono,T=Steinberg;Wavelab '
-                                                  '7; Normalize to 0dBFS / '
-                                                  'A=PCM,F=96000,W=24,M=mono,T=Steinberg;Wavelab '
-                                                  '7; Sonnox DeClicker',
-                      'FileExtension': 'wav',
-                      'FileSize': '89070016',
-                      'File_Created_Date': 'UTC 2019-09-10 20:33:51.669',
-                      'File_Created_Date_Local': '2019-09-10 16:33:51.669',
-                      'File_Modified_Date': 'UTC 2018-08-23 18:52:07.910',
-                      'File_Modified_Date_Local': '2018-08-23 14:52:07.910',
-                      'Format': 'Wave',
-                      'OriginalSourceForm': 'Grooved Disc; 12 inch; None',
-                      'OverallBitRate': '2304029',
-                      'OverallBitRate_Mode': 'CBR',
-                      'Producer': 'US, MiU-H',
-                      'StreamSize': '982',
-                      'extra': {'Producer_Reference': 'MiU-H_86746-SR-10-1-pm',
-                                'bext_Present': 'Yes'}},
-                     {'@type': 'Audio',
-                      'BitDepth': '24',
-                      'BitRate': '2304000',
-                      'BitRate_Mode': 'CBR',
-                      'Channels': '1',
-                      'CodecID': '1',
-                      'Delay': '0.000000000',
-                      'Delay_Source': 'Container (bext)',
-                      'Duration': '309.267',
-                      'Format': 'PCM',
-                      'Format_Settings_Endianness': 'Little',
-                      'Format_Settings_Sign': 'Signed',
-                      'SamplingCount': '29689632',
-                      'SamplingRate': '96000',
-                      'StreamSize': '89069034',
-                      'StreamSize_Proportion': '0.99999'}]}}"""
-
-        output = subprocess.check_output(["./tools/MediaInfo.exe", path_to_wav, "--Output=JSON"])
-        mediainfo_json = json.loads(output)
+        Uses Windows 64-bit BWF MetaEdit CLI to get BEXT CSV:
         
-        return mediainfo_json
+        FileName,Description,Originator,OriginatorReference,OriginationDate,OriginationTime,TimeReference (translated),TimeReference,BextVersion,UMID,LoudnessValue,LoudnessRange,MaxTruePeakLevel,MaxMomentaryLoudness,MaxShortTermLoudness,CodingHistory,IARL,IART,ICMS,ICMT,ICOP,ICRD,IENG,IGNR,IKEY,IMED,INAM,IPRD,ISBJ,ISFT,ISRC,ISRF,ITCH
+        R:\\BAroQUe\\2019012\\0648\\0648-SR-4\\0648-SR-4-1-2-am.wav,Paul Phillips (Tape No. 4),"US, MiU-H",MiU-H_0648-SR-4-1-am,2019-05-20,12:04:58,00:47:59.000,276384000,1,,,,,,,"A=ANALOGUE,M=mono,T=Studer A-810; 7.5 ips; open reel
+A=PCM,F=96000,W=24,M=mono,T=Antelope Audio;Orion 32;A/D",,,,,,,"Schreibeis, Ryan",,,,,,,,,Reel-to-reel; 7 inch; Sony; None; Polyester"""
 
-    def check_bext_metadatum_exists(self, path_to_wav, mediainfo_json, container_type, metadatum, extra=False):
-        if extra == True:
-            metadatum_to_check = [container["extra"].get(metadatum) for container in mediainfo_json["media"]["track"] if container["@type"] == container_type][0]
-        else:
-            metadatum_to_check = [container.get(metadatum) for container in mediainfo_json["media"]["track"] if container["@type"] == container_type][0]
-        if not metadatum_to_check:
+        bwfmetaedit_csv = subprocess.check_output(["./tools/bwfmetaedit.exe", "--out-core", path_to_wav])
+
+        return bwfmetaedit_csv
+
+    def check_bext_metadatum_exists(self, path_to_wav, row, metadatum):
+        if not row.get(metadatum):
             self.error(
                 path_to_wav,
                 self.item_id,
                 metadatum + " does not exist"
             )
-    
 
-    def check_bext_metadatum_value_is(self, path_to_wav, mediainfo_json, container_type, metadatum, value):
-        metadatum_value_to_check = [container[metadatum] for container in mediainfo_json["media"]["track"] if container["@type"] == container_type][0]
-        if metadatum_value_to_check != value:
+    def check_bext_metadatum_value_is(self, path_to_wav, row, metadatum, value):
+        self.check_bext_metadatum_exists(path_to_wav, row, metadatum)
+        if row.get(metadatum) and row[metadatum] != value:
             self.error(
                 path_to_wav,
                 self.item_id,
-                metadatum + " value of " + metadatum_value_to_check + " does not equal " + value
+                metadatum + " value of " + row[metadatum] + " does not equal " + value
             )
+
+    def check_bext_metadatum_value_is_datetime(self, path_to_wav, row, metadatum):
+        self.check_bext_metadatum_exists(path_to_wav, row, metadatum)
+        if row.get(metadatum) and not dateparser.parse(row[metadatum]):
+            self.error(
+                path_to_wav,
+                self.item_id,
+                metadatum + " value of " + row[metadatum] + " is not datetime"
+            )
+
+    def validate_bwfmetaedit_csv(self, path_to_wav, bwfmetaedit_csv):
+        with io.StringIO(bwfmetaedit_csv.decode()) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.check_bext_metadatum_exists(path_to_wav, row, "Description")
+                self.check_bext_metadatum_value_is(path_to_wav, row, "Originator", "US, MiU-H")
+                originator_reference = "MiU-H_" + os.path.splitext(os.path.split(path_to_wav)[1])[0]
+                self.check_bext_metadatum_value_is(path_to_wav, row, "OriginatorReference", originator_reference)
+                self.check_bext_metadatum_exists(path_to_wav, row, "OriginationDate")
+                self.check_bext_metadatum_exists(path_to_wav, row, "TimeReference")
+                self.check_bext_metadatum_exists(path_to_wav, row, "OriginationTime")
+                self.check_bext_metadatum_exists(path_to_wav, row, "CodingHistory")
     
     def validate_wav_bext_chunks(self):
         """ 
@@ -108,16 +81,5 @@ class WavBextChunkValidator(BaroqueValidator):
         for item in tqdm(self.project.items):
             paths_to_wavs = self.get_paths_to_wavs(item)
             for path_to_wav in paths_to_wavs: 
-                mediainfo_json = self.get_mediainfo_json(path_to_wav)
-                # Replicates functionality in "R:\AV Processing\TEST FILES\MDQC Templates\WAV_check_BEXT_SPECS_5.tpl"
-                self.check_bext_metadatum_value_is(path_to_wav, mediainfo_json, "Audio", "BitDepth", "24")
-                self.check_bext_metadatum_value_is(path_to_wav, mediainfo_json, "Audio", "Format", "PCM")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "Description")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "EncodedBy")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "Encoded_Date")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "Encoded_Library_Settings")
-                self.check_bext_metadatum_value_is(path_to_wav, mediainfo_json, "General", "FileExtension", "wav")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "OriginalSourceForm")
-                self.check_bext_metadatum_value_is(path_to_wav, mediainfo_json, "General", "Producer", "US, MiU-H")
-                self.check_bext_metadatum_exists(path_to_wav, mediainfo_json, "General", "Producer_Reference", extra=True)
-                self.check_bext_metadatum_value_is(path_to_wav, mediainfo_json, "Audio", "SamplingRate", "96000")
+                bwfmetaedit_csv = self.get_bwfmetaedit_csv(path_to_wav)
+                self.validate_bwfmetaedit_csv(path_to_wav, bwfmetaedit_csv)
