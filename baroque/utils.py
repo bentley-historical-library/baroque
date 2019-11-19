7439f0536532b0e007d3b681c2a4a013de209752
Hyeeyoung Kim <hyeeyoung.kim@gmail.com>
2019-11-19 11:01:07 -0500

    Update comments in sanitize_text()

--- baroque/utils.py
+++ baroque/utils.py
@@ -9,16 +9,16 @@ def sanitize_text(string):
     else:
         string = string.replace("&", "and")
 
-        # Replaces "runs of consecutive whitespace" to a single space AND removes any trailing space
+        # Replace "runs of consecutive whitespace" to single spaces AND remove trailing spaces
         # https://docs.python.org/3.8/library/stdtypes.html#str.split
         string = " ".join(string.split()).strip()
 
-        # Removes non-alphanumeric characters BUT keeps " " and "/" (for date format)
+        # Remove non-alphanumeric characters BUT keeps single spaces and "/" (for date format)
         # https://stackoverflow.com/questions/7406102/create-sane-safe-filename-from-any-unsafe-string
         keep_characters = (" ", "/")
         string = "".join(c for c in string if c.isalnum() or c in keep_characters)
 
-        # Normalizes unicode accents AND remove unicode-specific characters
+        # Normalize unicode accents AND remove unicode-specific characters
         # https://stackoverflow.com/questions/44431730/how-to-replace-accented-characters-in-python?rq=1
         # https://docs.python.org/3.8/library/unicodedata.html#unicodedata.normalize
         string = unicodedata.normalize('NFKC', string).encode('ascii', 'ignore').decode()
